12b67609ef9fde2d50e8aa76ea4eb8125f442ac7
Robin Munn <robin_munn@sil.org>
2013-11-26 17:46:06 +0700

    Parse all contours, not just one

--- src/extractpoints.py
+++ src/extractpoints.py
@@ -2,20 +2,24 @@
 
 import fontforge
 import optparse
+import argparse
 import sys
 import os, os.path
 import itertools
 
+DEFAULT_FONT='/usr/share/fonts/truetype/padauk/Padauk.ttf'
+DEFAULT_GLYPH='u1021'
+
 def parse_args():
     "Parse the arguments the user passed in"
-    parser = optparse.OptionParser(description="Demonstrate parsing arguments")
-    #parser.add_option('--help', help="Print the help string")
-    parser.add_option('-v', '--verbose', action="store_true", help="Give more verbose error messages")
-    parser.add_option('-i', '--input', action="store", dest="inputfilename", help="Font file (SFD or TTF format)")
-    parser.add_option('-g', '--glyph', action="store", dest="glyphname", help="Glyph name to extract")
-    opts, args = parser.parse_args()
-    opts.exportname = opts.glyphname + '.svg'
-    return opts, args
+    parser = argparse.ArgumentParser(description="Demonstrate parsing arguments")
+    #parser.add_argument('--help', help="Print the help string")
+    parser.add_argument('-v', '--verbose', action="store_true", help="Give more verbose error messages")
+    parser.add_argument("inputfilename", nargs="?", default=DEFAULT_FONT, help="Font file (SFD or TTF format)")
+    parser.add_argument("glyphname", nargs="?", default=DEFAULT_GLYPH, help="Glyph name to extract")
+    args = parser.parse_args()
+    args.exportname = args.glyphname + '.svg'
+    return args
 
 # Demo of how to extract the control points from a font.
 # Run "sudo apt-get install fonts-sil-padauk" before calling this function.
@@ -29,8 +33,15 @@ def extraction_demo(fname,letter):
     print "U+1015 has {} layer(s)".format(len(layer))
     # Result was 1: so there is exactly one contour. If there were more, we'd
     # loop through them each in turn.
-    contour = layer[0]
-    points = list(contour)
+    polylines = []
+    for contour in layer:
+        points = list(contour)
+        pointlist_with_midpoints = extrapolate_midpoints(points)
+        vectors = extractvectors(pointlist_with_midpoints)
+
+        polyline = ff_to_tuple(vectorpairs_to_pointlist(vectors))
+        polylines.append(polyline)
+    make_svg(polylines)
     return points
     # Note that there may be several off-curve points in a sequence, as with
     # the U+1015 example I chose here. The FontForge Python documentation at
@@ -49,55 +60,9 @@ def pairwise(source):
     for a, b in itertools.izip(source, source2):
         yield (a, b)
 
-def extractbeziers(points):
-    i=0
-    while i<len(points)-1:
-        # This appends a list of the two consecutive on-curve points with any off-curve points between them.
-        if points[i+1].on_curve:
-            added_bezier=points[i:i+2]
-            i=i+1
-        else:
-            added_bezier=points[i:i+3]
-            i=i+2
-        yield added_bezier
-        
-def extractvectors(points):
-    points = list(points)
-    for candidate in extractbeziers(points):
-        if len(candidate) == 2:
-            # It's a vector
-            yield candidate
-        else:
-            yield [candidate[0], candidate[-1]]
-          
-def vectorpairs_to_pointlist(pairs):
-    pairs = list(pairs)
-    return [pair[0] for pair in pairs] + [pairs[-1][-1]]
-
-def ff_to_tuple(ffpointlist):
-    return [(p.x, p.y) for p in ffpointlist]
-    
-def polydraw(points):
-    polylines = [points]
-    make_svg(polylines)
-    
-def make_svg(polylines):
-    import svgwrite
-    svg = svgwrite.Drawing(filename = "tryme.svg")
-    for polyline in polylines:
-        svgshape = svgwrite.shapes.Polyline(polyline, stroke="black", stroke_width=10, fill="white")
-    svg.add(svgshape)
-    svg.save()
-    import subprocess
-    print "About to load inkscape, please wait a bit..."
-    subprocess.call(['inkscape', 'tryme.svg'])
-    print "Did it work? You tell me, I'm just a computer so I can't tell."
-
-
-if __name__ == "__main__":
-    #opts, args = parse_args()
-    points=extraction_demo('/usr/share/fonts/truetype/padauk/Padauk.ttf',0x1021)
-    points.append(points[0])  # To close the curve
+def extrapolate_midpoints(points):
+    if not (points[-1] == points[0]):
+        points.append(points[0])  # Close the curve
     result = []
     for a, b in pairwise(points):
         if a.on_curve or b.on_curve:
@@ -110,14 +75,57 @@ if __name__ == "__main__":
     # Last point will not have been part of any pairs, so it won't have been
     # appended. Append it now.
     result.append(points[-1])
-    # If the first point (which is also the last point) is an off-curve
-    # point, then the last curve won't be completed, because you need to
-    # end with an on-curve point. Also if the first/last point in our point
-    # list is an off-curve point, then the last on-curve point must be the
-    # second point in result. This could happen, for instance, if our original
-    # points list was nothing but off-curve points (e.g.def polydraw(points):
-    
-    polydraw(ff_to_tuple(vectorpairs_to_pointlist(extractvectors(result))))
+    return result
+
+def extractbeziers(points):
+    i=0
+    while i<len(points)-1:
+        # This appends a list of the two consecutive on-curve points with any off-curve points between them.
+        if points[i+1].on_curve:
+            added_bezier=points[i:i+2]
+            i=i+1
+        else:
+            added_bezier=points[i:i+3]
+            i=i+2
+        yield added_bezier
+
+def extractvectors(points):
+    points = list(points)
+    for candidate in extractbeziers(points):
+        if len(candidate) == 2:
+            # It's a vector
+            yield candidate
+        else:
+            yield [candidate[0], candidate[-1]]
+
+def vectorpairs_to_pointlist(pairs):
+    pairs = list(pairs)
+    return [pair[0] for pair in pairs] + [pairs[-1][-1]]
+
+def ff_to_tuple(ffpointlist):
+    return [(p.x, p.y) for p in ffpointlist]
+
+def polydraw(points):
+    polylines = [points]
+    make_svg(polylines)
+
+def make_svg(polylines):
+    import svgwrite
+    svg = svgwrite.Drawing(filename = "tryme.svg")
+    for polyline in polylines:
+        svgshape = svgwrite.shapes.Polyline(polyline, stroke="black", stroke_width=10, fill="white")
+        svg.add(svgshape)
+    svg.save()
+    import subprocess
+    print "About to load inkscape, please wait a bit..."
+    subprocess.call(['inkscape', 'tryme.svg'])
+    print "Did it work? You tell me, I'm just a computer so I can't tell."
+
+
+if __name__ == "__main__":
+    #args = parse_args()  # Not yet used
+    extraction_demo('/usr/share/fonts/truetype/padauk/Padauk.ttf',0x104f)
+
 if False:
     import svgwrite
     svg = svgwrite.Drawing(filename = "tryme.svg")
