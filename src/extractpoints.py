1a8122359dd3cc35e2729a82dbbda1256e96af34
Robin Munn <robin_munn@sil.org>
2013-12-02 10:42:25 +0700

    Process triangles ourselves so we can get midpoints
    Instead of handing the data points off to python-poly2tri/test.py,
    we'll process the triangles ourselves (with cdt.triangulate()) so
    that we'll have the actual triangle data and can do things like
    figure out midpoints and connect them.
    
    Our code also now handles holes, so we're all set for drawing those
    midpoint-connecting line segments.

--- src/extractpoints.py
+++ src/extractpoints.py
@@ -6,6 +6,9 @@ import argparse
 import sys
 import os, os.path
 import itertools
+sys.path.append('../../python-poly2tri')
+import p2t
+sys.path.remove('../../python-poly2tri')
 
 DEFAULT_FONT='/usr/share/fonts/truetype/padauk/Padauk.ttf'
 DEFAULT_GLYPH='u1021'
@@ -44,29 +47,137 @@ def extraction_demo(fname,letter):
     padauk = fontforge.open(fname)
     global args
     args.em = padauk.em
+    if isinstance(letter, int):
+        codepoint = letter
+        glyph = padauk[codepoint]
+        # Should use args.glyphname for this...
+        letter = glyph.glyphname
+    if letter.startswith('U+'):
+        codepoint = int(glyphname[2:], 16)
+        glyph = padauk[codepoint]
+        # Should use args.glyphname for this...
+        letter = glyph.glyphname
     pa = padauk[letter] # U+1015 MYANMAR LETTER PA
     layer = pa.foreground
     print "U+1015 has {} layer(s)".format(len(layer))
     # Result was 1: so there is exactly one contour. If there were more, we'd
     # loop through them each in turn.
     polylines = []
+    holes = []
     for contour in layer:
         points = list(contour)
         pointlist_with_midpoints = extrapolate_midpoints(points)
         vectors = extractvectors(pointlist_with_midpoints)
 
-        polyline = ff_to_tuple(vectorpairs_to_pointlist(vectors))
-        polylines.append(polyline)
+        #polyline = ff_to_tuple(vectorpairs_to_pointlist(vectors))
+        polyline = vectorpairs_to_pointlist(vectors)
+        if contour.isClockwise():
+            polylines.append(polyline)
+        else:
+            holes.append(polyline)
     #make_svg(polylines)
-    savepoints(polylines[0])
-    import subprocess
-    subprocess.call(['python', '../../python-poly2tri/test.py', args.datfilename, '0', '0', '0.4'])
+    #savepoints(polylines[0])
+    #import subprocess
+    #subprocess.call(['python', '../../python-poly2tri/test.py', args.datfilename, '0', '0', '0.4'])
+    triangles = make_triangles(polylines, holes)
+    draw_all(polylines, holes, triangles)
     return points
     # Note that there may be several off-curve points in a sequence, as with
     # the U+1015 example I chose here. The FontForge Python documentation at
     # one point talks about "the TrueType idiom where an on-curve point mid-way
     # between its control points may be omitted, leading to a run of off-curve
     # points (with implied but unspecified on-curve points between them)."
+
+def flip_polyline(polyline):
+    result = []
+    for point in polyline:
+        try:
+            x, y = point.x, args.em-point.y
+            result.append(point.__class__(x,y))
+        except AttributeError:
+            x, y = point[0], args.em-point[1]
+            result.append(tuple(x,y))
+    return result
+
+def convert_polyline_to_polytri_version(polyline):
+    result = []
+    for point in polyline:
+        try:
+            x, y = point.x, point.y
+        except AttributeError:
+            x, y = point[0], point[1]
+        result.append(p2t.Point(x, y))
+    return result
+
+def make_triangles(polylines, holes=None):
+    if holes is None:
+        holes = []
+    triangles = []
+    for polyline in polylines:
+        new_polyline = convert_polyline_to_polytri_version(polyline)
+        del new_polyline[-1]
+        cdt = p2t.CDT(new_polyline)
+        for hole in holes:
+            del hole[-1]
+            cdt.add_hole(hole)
+        triangles.extend(cdt.triangulate())
+    return triangles
+
+def draw_all(polylines, holes, triangles):
+    import pygame
+    from pygame.gfxdraw import trigon, line
+    from pygame.locals import *
+    SCREEN_SIZE = (1280,800)
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE,0,8)
+    pygame.display.set_caption('Triangulation of glyph (name goes here)')
+    red = pygame.Color(255, 0, 0)
+    green = pygame.Color(0, 255, 0)
+    blue = pygame.Color(0, 0, 255)
+    ZOOM = 0.5  # Reduce size by half
+    for t in triangles:
+        x1 = int(t.a.x * ZOOM)
+        y1 = int((args.em-t.a.y) * ZOOM)
+        x2 = int(t.b.x * ZOOM)
+        y2 = int((args.em-t.b.y) * ZOOM)
+        x3 = int(t.c.x * ZOOM)
+        y3 = int((args.em-t.c.y) * ZOOM)
+        trigon(screen, x1, y1, x2, y2, x3, y3, red)
+
+    # Close the polylines loop again prior to drawing
+    for polyline in polylines:
+        polyline.append(polyline[0])
+        flipped = flip_polyline(polyline)
+        for a, b in pairwise(flipped):
+            x1 = int(a.x * ZOOM)
+            y1 = int(a.y * ZOOM)
+            x2 = int(b.x * ZOOM)
+            y2 = int(b.y * ZOOM)
+            line(screen, x1, y1, x2, y2, green)
+
+    # Same for holes
+    for hole in holes:
+        hole.append(hole[0])
+        flipped = flip_polyline(hole)
+        for a, b in pairwise(flipped):
+            x1 = int(a.x * ZOOM)
+            y1 = int(a.y * ZOOM)
+            x2 = int(b.x * ZOOM)
+            y2 = int(b.y * ZOOM)
+            line(screen, x1, y1, x2, y2, blue)
+
+    # Show result and wait for keypress
+    pygame.display.update()
+    done = False
+    while not done:
+        e = pygame.event.wait()
+        if (e.type == QUIT):
+            done = True
+            break
+        elif (e.type == KEYDOWN):
+            done = True
+            break
+
 
 def averagepoint(point1, point2):
     avgx = (point1.x + point2.x) / 2.0
@@ -150,6 +261,7 @@ def main():
     global args
     args = parse_args()
     extraction_demo('/usr/share/fonts/truetype/padauk/Padauk.ttf',0xaa75)
+    #extraction_demo('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf',0x0e3f)
     return 0
 
 if __name__ == "__main__":
