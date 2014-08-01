c95497787fbc4688fe080820afc2b6378eb9550b
Robin Munn <robin_munn@sil.org>
2014-08-01 14:43:39 +0700

    Tweak default dot radius one final time
    After testing, a dot radius of 12 seems to be a good default.

--- src/extractpoints.py
+++ src/extractpoints.py
@@ -716,7 +716,7 @@ def parse_args():
     parser.add_argument('-l', '--show-lines', action = "store_true", help = "Show the midlines of the glyph")
     parser.add_argument('-d', '--show-dots', action = "store_true", help = "Show the dots that make the dotted version")
     parser.add_argument('-g', '--show-glyph', action = "store_true", help = "Show the glyph outline")
-    parser.add_argument('-r', '--radius', action = "store", type = float, default = 15, help = "Radius of dots, in em units (default 15)")
+    parser.add_argument('-r', '--radius', action = "store", type = float, default = 12, help = "Radius of dots, in em units (default 12)")
     parser.add_argument('-s', '--spacing', action = "store", type = float, default = 6.0, help = "Spacing of dots, as a multiple of dot radius (default 6.0 for 600%%)")
     args = parser.parse_args()
     args.visualize = (args.show_triangles or args.show_lines or args.show_dots or args.show_glyph)
